939775bb12920ba520a12fefeddff260c46d7be7
Sui Yan <suyan@linkedin.com>
2014-11-19 14:55:35 -0800

    save button now works fine

--- js/save.js
+++ js/save.js
@@ -1,10 +1,27 @@
+var saved = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+var count = 0;
+var toggleSave = function(button, index) {
+	saved[index] = !saved[index];
+	if(saved[index]){
+		count++;
+		$(button).find('.fa').removeClass('fa-star-o').addClass('fa-star');
+		$(button).find('p').text('Saved');
+	} 
+	else {
+		count--;
+		$(button).find('.fa').removeClass('fa-star').addClass('fa-star-o');
+		$(button).find('p').text('Save');
+	}
+	if(count == 0) {
+		$(document).find('.toggle').hide();
+	}
+	else {
+		$(document).find('.toggle').show();
+		$(document).find('#save-count').text(count + ' ' + 'saved');		
+	}
+};
 $(document).ready( function() {
-	//var saveStats = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
-	// var activeTabId = $('.tab-pane').('.active').attr('id');
-	// var index = activeTabId.slice(8, activeTabId.length) - 1;
-	//$('#hero-star').find('.fa').addClass('fa-star-o');
-	// var saved = false;
-	// $('#hero-star').click(function(){
-	// 	saved = !saved;
-	// });
+	if(count == 0) {
+		$(document).find('.toggle').hide();
+	}
 });
