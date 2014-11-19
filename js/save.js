4a6043e5968af6041e9e4a32d88267d73232d409
Sui Yan <suyan@linkedin.com>
2014-11-18 20:19:56 -0800

    save button UI

---
+++ js/save.js
@@ -0,0 +1,10 @@
+$(document).ready( function() {
+	//var saveStats = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+	// var activeTabId = $('.tab-pane').('.active').attr('id');
+	// var index = activeTabId.slice(8, activeTabId.length) - 1;
+	//$('#hero-star').find('.fa').addClass('fa-star-o');
+	// var saved = false;
+	// $('#hero-star').click(function(){
+	// 	saved = !saved;
+	// });
+});
