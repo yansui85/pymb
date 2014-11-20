1b5964248b4489411e8054febdf11473ed640241
Sui Yan <suyan@linkedin.com>
2014-11-19 18:33:18 -0800

    added second page and made the first page better

--- js/save.js
+++ js/save.js
@@ -1,27 +1,35 @@
 var saved = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 var count = 0;
-var toggleSave = function(button, index) {
+var toggleSave = function(button) {
+	var activeItem = $('ul#tabs').children('.active');
+	var index = $('li').index(activeItem);
 	saved[index] = !saved[index];
 	if(saved[index]){
 		count++;
 		$(button).find('.fa').removeClass('fa-star-o').addClass('fa-star');
 		$(button).find('p').text('Saved');
+		$('ul#tabs').children('.active').find('.fa-star').show();
 	} 
 	else {
 		count--;
 		$(button).find('.fa').removeClass('fa-star').addClass('fa-star-o');
 		$(button).find('p').text('Save');
+		$('ul#tabs').children('.active').find('.fa-star').hide();
 	}
-	if(count == 0) {
-		$(document).find('.toggle').hide();
-	}
-	else {
-		$(document).find('.toggle').show();
-		$(document).find('#save-count').text(count + ' ' + 'saved');		
-	}
+	$('#save-count').text(count + ' ' + 'saved');
+	//following for hiding 0 counts
+	// if(count == 0) {
+	// 	$(document).find('.toggle').hide();
+	// }
+	// else {
+		// $(document).find('.toggle').show();
+		//$(document).find('#save-count').text(count + ' ' + 'saved');		
+	// }
 };
 $(document).ready( function() {
-	if(count == 0) {
-		$(document).find('.toggle').hide();
-	}
+	$('ul#tabs').find('.fa-star').hide();
+	$('#save-count').text(count + ' ' + 'saved');
+	// if(count == 0) {
+	// 	$(document).find('.toggle').hide();
+	// }
 });
